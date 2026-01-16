d99b67704523d34438f57cbba6f3a70bccaf2503
Nelson David Aristizabal Amaya <david.14420@gmail.com>
2026-01-16 13:13:27 -0500

    refactor: simplify PostCSS configuration by using dynamic imports for plugins

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,14 @@
+import tailwindcss from "tailwindcss";
+
+const plugins = [tailwindcss];
+
+try {
+  const autoprefixer = (await import("autoprefixer")).default;
+  plugins.push(autoprefixer);
+} catch (error) {
+  console.warn("Skipping autoprefixer: module not found.");
+}
+
 export default {
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
-  },
+  plugins,
 };
